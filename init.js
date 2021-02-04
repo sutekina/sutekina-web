4044e20fb64420e63b95f0ccce7143d1ae736a6f
twentythreetea <felixjcaldeira@gmail.com>
2021-02-04 15:38:35 +0100

    added more configurations and initializiaiton imporved yup cool changes ikr

--- init.js
+++ init.js
@@ -1,7 +1,9 @@
 const log = require("./utils/log");
 const fs = require("fs");
+const package = require("./package.json");
 
 let config;
+
 try {
     config = require("./config.json");
 } catch(e) {
@@ -25,10 +27,10 @@ try {
     });
 }
 
-(async() => {
+module.exports = new Promise(async(resolve, reject) => {
     // this code wait for config to be defined and then the shit after runs
     while(!config) await new Promise(resolve => setTimeout(resolve, 1000));
-
+    let modules_ready = false;
     if(config.debug.install_modules_on_boot) {
         log.info("Please wait shortly while we check for missing modules and install them.")
         const util = require('util');
@@ -38,10 +40,55 @@ try {
                 const { stdout, stderr } = await exec('npm install');
                 if(stdout) log.output(stdout);
                 if(stderr) log.error(stderr);
+                modules_ready = true;
             } catch (err) {
                 log.error(err);
             };
         };
         npm_install();
+    } else {
+        modules_ready = true
     }
-})();
+
+    while(!modules_ready) await new Promise(resolve => setTimeout(resolve, 1000));
+
+    let modules = {}
+    for(let i = 0; i < Object.keys(package.dependencies).length; i++) {
+        let key = Object.keys(package.dependencies)[i];
+        modules[key] = require(key);
+        log.info(`Successfully required "${key}"`)
+    }
+
+    log.info(`Successfully required all modules, connecting to MYSQL now.`)
+
+    const db_config = {
+        host : config.mysql.host,
+        user : config.mysql.user,
+        password : config.mysql.password,
+        database : config.mysql.database,
+        timezone: config.mysql.timezone,
+        insecureAuth : config.mysql.insecureAuth
+    }
+ 
+    let connection;
+
+    handleDisconnect = () => {
+        connection = modules.mysql2.createConnection(db_config);
+    
+        connection.connect(function(err) {
+            if(err) {
+                log.error('MYSQL CONNECT ERR ' + err);
+                setTimeout(handleDisconnect, 2000);
+            }
+        });
+    
+        connection.on('error', function(err) {
+            console.log('MYSQL ERR ' + err);
+            handleDisconnect();                         
+        });
+    }
+    
+    handleDisconnect();
+    
+    resolve([modules, config]);
+});
---
+++ main.js
@@ -0,0 +1,5 @@
+require("./init").then(([modules, config]) => {
+    console.log(Object.keys(modules));
+    console.log(config)
+
+}); 
